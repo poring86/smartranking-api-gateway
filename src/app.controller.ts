021c055eb8a1e384016c4ee4ab6ef98be35ed392
Matheus Lino <matheuslino86a@gmail.com>
2022-08-07 18:11:23 -0400

    event emitter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:password'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() CriarCategoriaDto: CriarCategoriaDto) {
+    return this.clientAdminBackend.emit('criar-categoria', CriarCategoriaDto);
   }
 }
