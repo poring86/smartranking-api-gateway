c40eb20be97e966b3253b8dcd42b2bb0cfdf483c
Matheus Lino <matheuslino86a@gmail.com>
2022-08-10 22:48:44 -0400

    Alterar usuário

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -23,7 +23,7 @@ export class AppController {
     this.clientAdminBackend = ClientProxyFactory.create({
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://poring86:test@localhost:5672/smartranking'],
+        urls: ['amqp://guest:guest@localhost:5672/smartranking'],
         queue: 'admin-backend',
       },
     });
@@ -32,11 +32,7 @@ export class AppController {
   @Post('categorias')
   @UsePipes(ValidationPipe)
   async criarCategoria(@Body() CriarCategoriaDto: CriarCategoriaDto) {
-    console.log('passou aqui');
-    try {
-      return this.clientAdminBackend.emit('criar-categoria', CriarCategoriaDto);
-    } catch (e) {
-      console.log(e);
-    }
+    console.log('CriarCategoriaDto', CriarCategoriaDto);
+    return this.clientAdminBackend.emit('criar-categoria', CriarCategoriaDto);
   }
 }
