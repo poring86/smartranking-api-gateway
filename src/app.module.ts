2e341b9e422bfd47ecd0784b3dac74e97707c6db
Matheus Lino <matheuslino86a@gmail.com>
2022-08-24 22:01:35 -0400

    Refatorando app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { CategoriasModule } from './categorias/categorias.module';
+import { ClientProxySmartRanking } from './proxyrmq/client-proxy';
+import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
 
 @Module({
-  imports: [CategoriasModule],
-  controllers: [AppController],
-  providers: [],
+  imports: [CategoriasModule, ProxyRMQModule],
+  controllers: [],
+  providers: [ClientProxySmartRanking],
 })
 export class AppModule {}
